2727f519b1e0eb7f0810d4d897771492c3036b5a
Sharad <sharad.rajore@zensar.com>
2025-09-10 12:03:44 +0530

    connected to open and closed llms

--- openai_langchain.py
+++ openai_langchain.py
@@ -1,6 +1,9 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
-llm = ChatOpenAI(api_key="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",model="gpt-4o-mini")
+load_dotenv(override=True)
+
+llm = ChatOpenAI()
 
 response = llm.invoke("What is the capital of India?")
 
